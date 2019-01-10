d1998fb6ac22b968129213cca5b0b91a3d67df55
Nick Thacker <nthacker@riskcontroltech.com>
2019-01-10 14:52:08 -0500

    Finish code for MNIST testing

--- DeepMNIST.py
+++ DeepMNIST.py
@@ -43,12 +43,79 @@ b_conv1 = bias_variable([32])
 # Do convolution on images, add bias and push through the 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 # take results and run through max_pool
-hpool1 = max_pool_2x2(h_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
 
 # 2nd Convolution layer
 # Process the 32 features from Convolution layer 1, in 5 x 5 patch.  Return 64 features weights and biases
 W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([32])
+b_conv2 = bias_variable([64])
 # Do convolution of the output of the 1st convolution layer.  Pool results
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
+
+# Fully Connected Layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+# Connect output of pooling layer 2 as input to fully connected layer
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# dropout some neurons to reduce overfitting
+keep_prob = tf.placeholder(tf.float32)  # get dropout probability as a training input.
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# Readout layer
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+# Define model
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+# Loss measurement
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
+
+# Loss optimization
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+# What is correct
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_,1))
+
+# How accuract is it?
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Initialize all of the variables
+sess.run(tf.global_variables_initializer())
+
+# Train the model
+import time
+
+# define number of steps and how often we display progress
+num_steps = 3000
+display_every = 100
+
+# Start time
+start_time = time.time()
+end_time = time.time()
+
+for i in range(num_steps):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    # Periodic status display
+    if i%display_every == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0], y_: batch[1], keep_prob: 1.0})
+        end_time = time.time()
+        print("step {0}, elapsed time {1:.2f} seconds, training accuracy {2:.3f}%".format(i, end_time-start_time, train_accuracy * 100))
+
+
+# Display summary
+# Time to train!
+end_time = time.time()
+print("Total training time for {0} batches: {1:.2f} seconds".format(i + 1, end_time-start_time))
+
+# Accuracy on test data
+print("Test accuracy {0:.3f}%".format(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}) * 100.0))
+
+sess.close()
